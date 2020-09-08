d1d071eb09802b8caf1f2bcc6da7aa4b805748d5
Stefan Erwin Baumer <baumeredv@gmail.com>
2020-09-08 21:51:26 +0200

    removed unused imports

--- src/test/java/com/baumeredv/kanbanboard/AppTest.java
+++ src/test/java/com/baumeredv/kanbanboard/AppTest.java
@@ -2,8 +2,6 @@ package com.baumeredv.kanbanboard;
 
 
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppTest {
 
